f9ce8ccdf4a8104262f3fa2a3de65d7c9776d99f
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-06-01 23:32:53 +0300

    Generic Yapıyı Classlara Uygulamak 2

---
+++ EReconciliation.DataAccess/Concrete/EntityFramework/EfUserCompanyDal.cs
@@ -0,0 +1,11 @@
+﻿using EReconciliation.Core.DataAccess.EntityFrameowork;
+using EReconciliation.Core.Entities.Concrete;
+using EReconciliation.DataAccess.Abstract;
+using EReconciliation.DataAccess.Concrete.EntityFramework.Context;
+
+namespace EReconciliation.DataAccess.Concrete.EntityFramework
+{
+    public class EfUserCompanyDal : EfEntityRepositoryBase<UserCompany, ContextDb>, IUserCompanyDal
+    {
+    }
+}
---
+++ EReconciliation.DataAccess/Concrete/EntityFramework/EfUserOperationClaimDal.cs
@@ -0,0 +1,11 @@
+﻿using EReconciliation.Core.DataAccess.EntityFrameowork;
+using EReconciliation.Core.Entities.Concrete;
+using EReconciliation.DataAccess.Abstract;
+using EReconciliation.DataAccess.Concrete.EntityFramework.Context;
+
+namespace EReconciliation.DataAccess.Concrete.EntityFramework
+{
+    public class EfUserOperationClaimDal : EfEntityRepositoryBase<UserOperationClaim, ContextDb>, IUserOperationClaimDal
+    {
+    }
+}
