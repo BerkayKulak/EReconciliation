cdeda291d5757f8b059962affe83af54cfad0ba7
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-06-04 19:44:38 +0300

    IAuthService

---
+++ EReconciliation.Business/Abstract/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using EReconciliation.Core.Entities.Concrete;
+using EReconciliation.Core.Utilities.Results.Abstract;
+using EReconciliation.Core.Utilities.Security.JWT;
+using EReconciliation.Entities.Dtos;
+
+namespace EReconciliation.Business.Abstract
+{
+    public interface IAuthService
+    {
+        IDataResult<User> Register(UserForRegister userForRegister, string password);
+        IDataResult<User> Login(UserForRegister userForRegister);
+        IResult UserExists(string email);
+        IDataResult<AccessToken> CreateAccessToken(User user);
+    }
+}
