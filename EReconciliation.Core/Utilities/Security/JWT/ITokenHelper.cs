c62b954a855f0820a58ee61eeab98011ddea434c
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-06-04 19:24:04 +0300

    Access Token, Token Helpter, Security Key Helper Yazmak

---
+++ EReconciliation.Core/Utilities/Security/JWT/ITokenHelper.cs
@@ -0,0 +1,9 @@
+﻿using EReconciliation.Core.Entities.Concrete;
+
+namespace EReconciliation.Core.Utilities.Security.JWT
+{
+    public interface ITokenHelper
+    {
+        AccessToken CreateToken(User user, List<OperationClaim> operationClaims, int companyId);
+    }
+}
