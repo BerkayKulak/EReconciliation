a99f09399f28d6e5e3894271d0024dac41b3d059
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-06-04 13:17:20 +0300

    SuccessResult, SucessDataResult, ErrorResult, ErrorDataResult

---
+++ EReconciliation.Core/Utilities/Results/Concrete/ErrorResult.cs
@@ -0,0 +1,13 @@
+﻿namespace EReconciliation.Core.Utilities.Results.Concrete
+{
+    public class ErrorResult : Result
+    {
+        public ErrorResult() : base(false)
+        {
+        }
+
+        public ErrorResult(string message) : base(false, message)
+        {
+        }
+    }
+}
