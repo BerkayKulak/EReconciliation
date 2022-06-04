1c19c136f4c13bb14d2837eb8c172d55b58c7c75
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-06-04 20:43:50 +0300

    Register İşlemi 1

---
+++ EReconciliation.API/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using EReconciliation.Business.Abstract;
+using EReconciliation.Entities.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EReconciliation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(UserForRegister userForRegister)
+        {
+            var userExists = _authService.UserExists(userForRegister.Email);
+            if (!userExists.Success)
+            {
+                return BadRequest(userExists.Message);
+            }
+
+            var registerResult = _authService.Register(userForRegister, userForRegister.Password);
+            //var result = _authService.CreateAccessToken();
+            if (registerResult.Success)
+            {
+                return Ok(registerResult);
+            }
+
+            return BadRequest(registerResult.Message);
+        }
+
+    }
+}
