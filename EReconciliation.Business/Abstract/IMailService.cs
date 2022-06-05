908a48524d55dae48ffaf256a66ed31f0fcb082d
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-06-05 13:36:58 +0300

    Generic Mail Yapısı Oluşturma

---
+++ EReconciliation.Business/Abstract/IMailService.cs
@@ -0,0 +1,9 @@
+﻿using EReconciliation.Core.Utilities.Results.Abstract;
+
+namespace EReconciliation.Business.Abstract
+{
+    public interface IMailService
+    {
+        IResult SendMail(string mail, string subject, string body);
+    }
+}
