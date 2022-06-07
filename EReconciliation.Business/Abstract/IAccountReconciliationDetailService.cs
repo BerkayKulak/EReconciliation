ada7c01322ead6820697b334a1c0165e29c2bfb9
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-06-07 18:47:35 +0300

    Cari Mutabakat Detay Ekleme, Silme, Güncelleme, Excel'den Veri

--- EReconciliation.Business/Abstract/IAccountReconciliationDetailService.cs
+++ EReconciliation.Business/Abstract/IAccountReconciliationDetailService.cs
@@ -1,6 +1,15 @@
-﻿namespace EReconciliation.Business.Abstract
+﻿using EReconciliation.Core.Utilities.Results.Abstract;
+using EReconciliation.Entities.Concrete;
+
+namespace EReconciliation.Business.Abstract
 {
     public interface IAccountReconciliationDetailService
     {
+        IResult Add(AccountReconciliationDetail accountReconciliationDetail);
+        IResult AddToExcel(string filePath, int companyId);
+        IResult Update(AccountReconciliationDetail accountReconciliationDetail);
+        IResult Delete(AccountReconciliationDetail accountReconciliationDetail);
+        IDataResult<AccountReconciliationDetail> GetById(int id);
+        IDataResult<List<AccountReconciliationDetail>> GetList(int accountReconciliationId);
     }
 }
